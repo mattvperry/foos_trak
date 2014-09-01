c992b63b91c57d81d9f6a94b4372b2392d6a5f36
Matthew Perry <muffinman616@gmail.com>
2014-08-31 23:05:14 -0400

    Add sticky footer with copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,29 @@ module ApplicationHelper
     current_user.participated_in?(game) or current_user.created_game?(game)
   end
 
+  # Cannot find a place to put this =( (saving for later)
+  def github_button(type)
+    url = URI::HTTP.build({
+      host: "ghbtns.com",
+      path: "/github-btn.html",
+      query: {
+        user: :perrym5,
+        repo: :foos_trak,
+        type: type,
+        count: true,
+      }.to_query
+    }).to_s
+
+    haml_tag(:iframe, {
+      src: url,
+      allowtransparency: true,
+      frameborder: 0,
+      scrolling: 0,
+      width: 95,
+      height: 20
+    })
+  end
+
   def nav_link(text, path)
     class_name = current_page?(path) ? 'active' : ''
 
